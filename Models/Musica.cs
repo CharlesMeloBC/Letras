4b0fc765e8fe0c80b819099a0ec23a4c2427c19d
Charles <charles.melo145@gmail.com>
2024-12-23 16:23:08 -0300

    criando entidade modelo

---
+++ Models/Musica.cs
@@ -0,0 +1,11 @@
+﻿namespace Letrasdemusicas.Models
+{
+    public class Musica
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Letra { get; set; }
+    }
+}
