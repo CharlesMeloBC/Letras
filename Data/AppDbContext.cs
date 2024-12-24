72e8cfccf3b3d2c296f79708b5e9a32a5e61685d
Charles <charles.melo145@gmail.com>
2024-12-23 22:12:50 -0300

    Implementei conexão com o banco de dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Letrasdemusicas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Letrasdemusicas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Musica> Musicas { get; set; }
+    }
+}
